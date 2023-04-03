ea9bc88163c1507e65f6c425843a6c7f943c0e88
Tamás Tóth <tamas.toth@helsinki.fi>
2023-04-03 18:58:20 +0300

    basic login/register/quit view

--- src/index.py
+++ src/index.py
@@ -1,11 +1,47 @@
-from tkinter import Tk
+#!/usr/bin/env python3
+import tkinter as tk
 
-def main():
-    window = Tk()
-    window.title("SVG maestro")
+# Messagebox is not visible via above tk.messagebox...
+from tkinter import messagebox as msg
 
-    window.mainloop()
+# this will be backed by a table in sqlite
+users = {'test':'test'}
 
 
-if __name__ == "__main__":
-    main()
+class Application(tk.Frame):
+
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        tk.Label(self, text='username:').grid(row = 0, column = 0, sticky = tk.W, pady = 2)
+        self.username = tk.Entry(self)
+        self.username.grid(row = 0, column = 1, pady = 2)
+        
+        tk.Label(self, text='password:').grid(row = 1, column = 0, sticky = tk.W, pady = 2)
+        self.password = tk.Entry(self, show="*")
+        self.password.grid(row = 1, column = 1, pady = 2)
+
+
+        tk.Button(self, text='Login/register', command=self.processInput).grid()
+        tk.Button(self, text='Quit', command=self.quit).grid()
+    
+    def processInput(self):
+        user = self.username.get()
+        pw = self.password.get()
+        if user not in users:
+            users[user] = pw
+            msg.showinfo("Registration", "user successfully created")
+        elif users[user] != pw:
+            msg.showerror("Login failed", "wrong password")
+        else:
+            msg.showinfo("Login succeeded", "you're in!")
+            pass
+        
+
+
+app = Application()
+app.master.title('SVG artistic program')
+app.mainloop()
