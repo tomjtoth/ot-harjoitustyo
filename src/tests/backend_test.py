10804dc1c638b49fa41ec0bc7c7ce57d52466ded
Tamás Tóth <tamas.toth@helsinki.fi>
2023-04-19 14:59:16 +0300

    toinen test

--- src/tests/backend_test.py
+++ src/tests/backend_test.py
@@ -38,4 +38,4 @@ class TestDrawing(unittest.TestCase):
         self.backend.login_register("user", "user")
 
     def test_drawing(self):
-        "ésu disable gnome-keyring daemon.{service,socket}"
+        pass
